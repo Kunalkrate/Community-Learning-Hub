257b4c716f98fa92bc0426213beb8ac60e1257d5
kunalkrate <kunalkrate1973@gmail.com>
2025-05-03 14:42:08 +0530

    first commit

---
+++ server/data/index.js
@@ -0,0 +1,105 @@
+import mongoose from "mongoose";
+
+const userIds = [
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+];
+
+export const users = [
+  {
+    _id: userIds[0],
+    firstName: "Kunal",
+    lastName: "Kumar",
+    email: "kunalkrate@gmail.com",
+    password: "$2b$10$EYfW13g7JQvpHqFIkzjfd.pKW7C1m.dIF1Ye8/1aSQ0/OPhuK87Ra",
+    picturePath: "Photo with suit & tie.png",
+    friends: [],
+    location: "Ranchi, Jharkhand",
+    occupation: "Software Engineer",
+    viewedProfile: 14561,
+    impressions: 888822,
+    createdAt: 1115211422,
+    updatedAt: 1115211422,
+    __v: 0,
+  },
+  {
+    _id: userIds[1],
+    firstName: "Bitu",
+    lastName: "Don",
+    email: "bitudon@gmail.com",
+    password: "$2b$10$EYfW13g7JQvpHqFIkzjfd.pKW7C1m.dIF1Ye8/1aSQ0/OPhuK87Ra",
+    picturePath: "Photo with suit & tie.png",
+    friends: [],
+    location: "Chapra,Bihar",
+    occupation: "Machenical Engineer",
+    viewedProfile: 12351,
+    impressions: 55555,
+    createdAt: 1595589072,
+    updatedAt: 1595589072,
+    __v: 0,
+  },
+  {
+    _id: userIds[2],
+    firstName: "Some",
+    lastName: "Guy",
+    email: "someguy@gmail.com",
+    password: "$2b$10$EYfW13g7JQvpHqFIkzjfd.pKW7C1m.dIF1Ye8/1aSQ0/OPhuK87Ra",
+    picturePath: "p4.jpeg",
+    friends: [],
+    location: "Canada, CA",
+    occupation: "Data Scientist Hacker",
+    viewedProfile: 45468,
+    impressions: 19986,
+    createdAt: 1288090662,
+    updatedAt: 1288090662,
+    __v: 0,
+  },
+  {
+    _id: userIds[3],
+    firstName: "Whatcha",
+    lastName: "Doing",
+    email: "whatchadoing@gmail.com",
+    password: "$2b$10$EYfW13g7JQvpHqFIkzjfd.pKW7C1m.dIF1Ye8/1aSQ0/OPhuK87Ra",
+    picturePath: "p6.jpeg",
+    friends: [],
+    location: "Korea, CA",
+    occupation: "Educator",
+    viewedProfile: 41024,
+    impressions: 55316,
+    createdAt: 1219214568,
+    updatedAt: 1219214568,
+    __v: 0,
+  },
+
+];
+
+export const posts = [
+
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[3],
+    firstName: "Whatcha",
+    lastName: "Doing",
+    location: "Korea, CA",
+    description:
+      "Another really long random description. This one is longer than the previous one.",
+    picturePath: "post2.jpeg",
+    userPicturePath: "p6.jpeg",
+    likes: new Map([
+      [userIds[1], true],
+      [userIds[2], true],
+    ]),
+    comments: [
+      "one more random comment",
+      "and another random comment",
+      "no more random comments",
+      "I lied, one more random comment",
+    ],
+  },
+];
